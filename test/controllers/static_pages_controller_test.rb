137a0c57c6e975096db4303a8989ffae46fcb3d6
Aaron Foucheaux <afoucheaux@gmail.com>
2021-10-03 08:28:36 -0500

    updated user test with string validation.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
@@ -32,7 +32,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
