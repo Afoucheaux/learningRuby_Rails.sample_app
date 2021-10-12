27d7017569ee00190e301309e35fc4add0a55b33
Aaron Foucheaux <afoucheaux@gmail.com>
2021-10-12 12:39:04 -0500

    Update session controler with flash input error message

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,10 +3,15 @@ class SessionController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+    flash[:danger] = 'Invalid email/password combination'
     render 'new'
+    end
   end
 
   def destroy
   end
-  
+
 end
