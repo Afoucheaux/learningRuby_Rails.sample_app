550af15db4929b415f130a3c009508bdad8dc13c
Aaron Foucheaux <afoucheaux@gmail.com>
2021-10-12 15:28:15 -0500

    stubbed user and testing update

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,13 +2,20 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-  test 'login with invalid infromation' do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'session/new'
-    post login_path, params: { session: {email: "", password: ""}}
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
     assert_template 'session/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
+
+
 end
