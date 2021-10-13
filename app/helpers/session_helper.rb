2f28dfbb83e6c5482294c304d492677295a53a46
Aaron Foucheaux <afoucheaux@gmail.com>
2021-10-13 12:39:29 -0500

    syntax update

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -7,17 +7,18 @@ module SessionHelper
   def remember(user)
     user.remember
     cookies.permanent.encrypted[:user_id] = user.id
-    cookies.permanent[:remeber_token] = user.remeber_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+     @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.encrypted[:user_id])
-      user = User.find_by(id: user_id)
-    if user && user.authenticated?(cookies[:remember_token])
-      log_in user
-      @current_user = user
+     user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -25,7 +26,14 @@ module SessionHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
